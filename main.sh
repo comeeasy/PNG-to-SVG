da032d81426b2d1227d9c48ae3975d1fcbcaec93
comeeasy <zz1236zz@naver.com>
2024-10-27 17:01:17 +0000

    [update] 목적에 맞게 수정 - no resize - bezier 그대로 사용

--- main.sh
+++ main.sh
@@ -3,8 +3,7 @@
 echo "Various steps to get the final svg format output."
 
 echo "01 - thresholding"
-# python ./Src/01-ResizeThreshold.py --inDir "./Input" --outDir "./Src/temp/01" --ResizeFlag False --ResizeH 4096 --ResizeW 4096
-python ./Src/01-ResizeThreshold.py --inDir $1 --outDir "./Src/temp/01" --ResizeFlag False --ResizeH 4096 --ResizeW 4096
+python ./Src/01-ResizeThreshold.py --inDir $1 --outDir "./Src/temp/01"
 
 echo "02 - png2ppm"
 inputdir='./Src/temp/01/'
@@ -40,8 +39,11 @@ python ./Src/04-svgCleaning.py --inDir "./Src/temp/03/" --outDir "./Src/temp/04"
 echo "05 - svg - 1M - 1Path"
 python ./Src/05-svg1M1Path.py --inDir "./Src/temp/04/" --outDir "./Src/temp/05"
 
+echo "06 - svg - 06-SmallPathRemoval"
+python ./Src/06-SmallPathRemoval.py --inDir "./Src/temp/05/" --outDir "./Src/temp/06"
+
 echo "07 - svg - longest path first"
-python ./Src/07-LongestPathFirst.py --inDir "./Src/temp/05/" --outDir "./Src/temp/07"
+python ./Src/07-LongestPathFirst.py --inDir "./Src/temp/06/" --outDir "./Src/temp/07"
 
 echo "08 - svg - reorder paths: nearest starting point."
 python ./Src/08-ReorderPath-Mat.py --inDir "./Src/temp/07/" --outDir "./Src/temp/08"
@@ -50,24 +52,11 @@ echo "09 - svg file cleaning"
 python ./Src/09-svgCleaning.py --inDir "./Src/temp/08/" --outDir "./Src/temp/09"
 
 echo "10 - svg - TUBerlin style"
-python ./Src/10-TUBerlinStyleSvg.py --inDir "./Src/temp/09/" --outDir "./Output/svg" --refSVG "./Src/Ref-svg.svg" --imgSize 800 --strokeWidth 1.8
-
-echo "11 - svg2png"
-inputdir='./Output/svg/'
-outputdir='./Output/png/'
+outputdir=$2
 if [ ! -e $"$outputdir" ]
 then
    mkdir $"$outputdir"
 fi
-for g in $inputdir*
-do
-    FILE=$(basename "${g}")
-    FILE1="${FILE%%.*}"
-    echo $outputdir$FILE1
-    convert $g -colorspace Gray -resize 256x256 $outputdir$FILE1".png"
-done
+python ./Src/10-TUBerlinStyleSvg.py --inDir "./Src/temp/09/" --outDir $outputdir --refSVG "./Src/Ref-svg.svg" --strokeWidth 1.0
 
-echo "12 - bèzier to line segments for training"
-python bezier2lineseg.py $2
-
-# rm -rf Src/temp/*
+rm -rf ./Src/temp/*
